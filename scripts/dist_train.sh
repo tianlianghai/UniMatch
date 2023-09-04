90353b3a68c4168bf76d7641192f6fa9f0552f74
tian lianghai <tianlhcs@163.com>
2023-09-04 17:11:04 +0000

    Default commit message

--- scripts/dist_train.sh
+++ scripts/dist_train.sh
@@ -17,7 +17,11 @@ unlabeled_id_path=splits/$dataset/$split/unlabeled.txt
 save_path=exp/$dataset/$method/$exp/$split
 
 mkdir -p $save_path
-python \
+
+torchrun \
+    --nproc_per_node=$1 \
+    --master_addr=localhost \
+    --master_port=$2 \
     $method.py \
     --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
     --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log
